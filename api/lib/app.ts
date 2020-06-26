c4e3ff5d313c2bd666391a6756ee1d69695af8d6
HommyDeJesus <HommyDeJesus@DESKTOP-A7S1VC8>
2020-06-26 17:02:15 -0400

    Api service added Added person list view Edit view Delete functionality Create functionality GetById functionality

--- api/lib/app.ts
+++ api/lib/app.ts
@@ -1,3 +1,4 @@
+import * as cors from "cors";
 import * as express from "express";
 import * as bodyParser from "body-parser";
 import { Routes } from "../lib/routes/Routes";
@@ -7,21 +8,38 @@ class App {
 
     public app: express.Application;
     public routes: Routes = new Routes();
-    public mongoUrl: string = 'mongodb://localhost:27017/person-crud';  
+    public mongoUrl: string = 'mongodb://localhost:27017/person-crud';
 
     constructor() {
         this.app = express();
-        this.config();        
-        this.routes.routes(this.app);     
+        this.config();
+        this.routes.routes(this.app);
         this.mongoSetup();
     }
 
-    private config(): void{
+    private config(): void {
         this.app.use(bodyParser.json());
         this.app.use(bodyParser.urlencoded({ extended: false }));
+
+        //options for cors midddleware
+        const options: cors.CorsOptions = {
+            allowedHeaders: ["Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"],
+            credentials: true,
+            methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
+            origin: [/.*localhost.*/],
+            preflightContinue: false
+        };
+
+        //use cors middleware
+        this.app.use(cors(options));
+
+        //add your routes
+
+        //enable pre-flight
+        this.app.options("*", cors(options));
     }
 
-    private mongoSetup(): void{
+    private mongoSetup(): void {
         mongoose.Promise = global.Promise;
         mongoose.connect(this.mongoUrl);
     }
--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,20 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from "@angular/common/http";
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PersonCreateComponent } from './components/person-create/person-create.component';
+import { PersonEditComponent } from './components/person-edit/person-edit.component';
+import { PersonListComponent } from './components/person-list/person-list.component';
+import { ApiService } from './service/api.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PersonCreateComponent
+    PersonCreateComponent,
+    PersonEditComponent,
+    PersonListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
