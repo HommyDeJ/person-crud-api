0aa0f6f13fc1975432ebf55032b4a584f4358ec3
HommyDeJesus <HommyDeJesus@DESKTOP-A7S1VC8>
2020-06-28 22:32:10 -0400

    ..

--- lib/app.ts
+++ lib/app.ts
@@ -21,24 +21,18 @@ class App {
     private config(): void {
         this.app.use(bodyParser.json());
         this.app.use(bodyParser.urlencoded({ extended: false }));
-
-        //options for cors midddleware
-        const options: cors.CorsOptions = {
-            allowedHeaders: ["Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept", "*"],
-            credentials: true,
-            methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
-            origin: ["https://person-crud-newtech-frontend.herokuapp.com/persons-list"],
-            preflightContinue: false
-        };
-
-        //use cors middleware
-        this.app.use(cors(options));
+        this.app.use(cors());
+        //env
         dotenv.config();
 
-        //add your routes
+        // Add headers
+        this.app.use(function(req, res, next) {
+            res.header("Access-Control-Allow-Origin", "*");
+            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+            next();
+          });
 
-        //enable pre-flight
-        this.app.options("*", cors(options));
+
     }
 
     private mongoSetup(): void {
